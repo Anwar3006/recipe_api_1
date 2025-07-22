ea9e0f0d627e6864a77243dbf79878087139200a
Anwar3006 <anwarsadat.d2@gmail.com>
2025-07-22 21:00:09 +0000

    feature: start cron job in production

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,19 +1,21 @@
 import cron from "cron";
-import https from https;
+import https from "https";
 
 import logger from "./logger.js";
-import {API_URL, VERSION} from "./env.js"
+import { API_URL, VERSION } from "./env.js";
 
-const job = new cron.CronJob('*/14 * * * *', function (){
-    https.get(`${API_URL}/api/${VERSION}/health`, (res) => {
-        if(res.statusCode === 200){
-            logger.info("Cron job executed successfully ✅");
-        } else {
-            logger.error("Cron job failed ❌");
-        }
-    }).on('error', (error) => {
-        logger.error(`Cron job failed: ${error.message} ❌`);
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get(`${API_URL}/api/${VERSION}/health`, (res) => {
+      if (res.statusCode === 200) {
+        logger.info("Cron job executed successfully ✅");
+      } else {
+        logger.error("Cron job failed ❌");
+      }
     })
-})
+    .on("error", (error) => {
+      logger.error(`Cron job failed: ${error.message} ❌`);
+    });
+});
 
-export default job
+export default job;
