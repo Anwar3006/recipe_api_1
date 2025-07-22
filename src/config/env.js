82fdafb6c0279c509d6ee2ef0bb1b6c4153728c4
Anwar3006 <anwarsadat.d2@gmail.com>
2025-07-22 21:02:22 +0000

    feature: start cron job in production

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,4 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const { PORT, NODE_ENV, VERSION, DATABASE_URL } = process.env;
+export const { PORT, NODE_ENV, VERSION, DATABASE_URL, API_URL } = process.env;
