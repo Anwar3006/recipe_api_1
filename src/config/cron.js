d2969ffcc41425d7d907e2f39cbafea0057b5c63
Anwar3006 <anwarsadat.d2@gmail.com>
2025-07-22 20:46:41 +0000

    feature: added cron job to prevent render cold starts

---
+++ src/config/cron.js
@@ -0,0 +1,19 @@
+import cron from "cron";
+import https from https;
+
+import logger from "./logger.js";
+import {API_URL, VERSION} from "./env.js"
+
+const job = new cron.CronJob('*/14 * * * *', function (){
+    https.get(`${API_URL}/api/${VERSION}/health`, (res) => {
+        if(res.statusCode === 200){
+            logger.info("Cron job executed successfully ✅");
+        } else {
+            logger.error("Cron job failed ❌");
+        }
+    }).on('error', (error) => {
+        logger.error(`Cron job failed: ${error.message} ❌`);
+    })
+})
+
+export default job
